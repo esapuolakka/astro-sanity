9477d12c0b6c553f24816e2dbf1ceae1bccf7194
Esa Puolakka <130772020+esapuolakka@users.noreply.github.com>
2025-08-29 17:40:42 +0300

    fix2

--- sanity.config.ts
+++ sanity.config.ts
@@ -6,6 +6,6 @@ export const client = createClient({
   apiVersion: '2025-08-29',
   useCdn: false,
   stega: {
-    studioUrl: "https://astro-esa.sanity.studio/",
+    studioUrl: "/studio",
   },
 });
